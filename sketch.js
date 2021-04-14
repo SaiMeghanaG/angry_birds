e5e83fa604cc9cce8512af81977d0842c651221d
SaimeghanaG <garudadrisaimeghana@gmail.com>
2021-04-14 14:16:10 +0530

    platform to ground

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform;
+var ground;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +17,7 @@ function setup(){
     world = engine.world;
 
     
-    platform = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -46,7 +46,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    platform.display();
+    ground.display();
     pig1.display();
     log1.display();
 
